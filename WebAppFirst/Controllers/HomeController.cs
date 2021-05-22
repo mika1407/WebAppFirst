19735d9cf61fd4d979a3a051c53b750b7edda87e
mika1407 <koneautomaatio@hotmail.com>
2021-05-22 04:31:01 +0300

    Add project files.

---
+++ WebAppFirst/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppFirst.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Tietoa näistä sivuista. / Your application description page.";
+            ViewBag.Herja = "Ole huolellinen, ettei tule virheitä!";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Minun yhteystiedot. / Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Map()
+        {
+            ViewBag.Message = "Saapumisohje / Map.";
+
+            return View();
+        }
+    }
+}
---
+++ WebAppFirst/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppFirst.Models;
+
+namespace WebAppFirst.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            northwindEntities db = new northwindEntities();
+            List<Products> model = db.Products.ToList();
+            db.Dispose();
+
+            return View(model);
+        }
+    }
+}
