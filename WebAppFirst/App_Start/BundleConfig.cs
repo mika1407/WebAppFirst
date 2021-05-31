db8de3e99f8aa93350ffef661c11703120789e50
mika1407 <koneautomaatio@hotmail.com>
2021-05-31 10:15:45 +0300

    Site.css kursorin muutos lisätty mm.

--- WebAppFirst/App_Start/BundleConfig.cs
+++ WebAppFirst/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace WebAppFirst
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstraplux.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
